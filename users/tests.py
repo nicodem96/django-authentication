3dc2c6bb66b9a1fa4120599dd0b1e66141401039
Nicola <nicolademuru@gmail.com>
2023-09-18 20:11:38 +0200

    aggiunti test- test passati

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 from django.contrib.auth import get_user_model
@@ -25,3 +26,23 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+class SignupPageTests(TestCase): # new
+    username = "newuser"
+    email = "newuser@email.com"
+
+    def setUp(self):
+        url = reverse("account_signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "account/signup.html")
+        self.assertContains(self.response, "Registrati")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
